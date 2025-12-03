b65ed47e2f0febaaeea7d08d0d5fc6e004aaf87b
qigoh <qigoh@s.sfusd.edu>
2025-12-03 11:41:55 -0800

    M9_L11 progress

--- M09.top_down_movement_workbook/lessons/pause_screen.gd
+++ M09.top_down_movement_workbook/lessons/pause_screen.gd
@@ -4,7 +4,9 @@ extends Control
 @onready var _ui_panel_container: PanelContainer = %UIPanelContainer
 @export_range(0.0,1.0) var menu_opened_amount := 0.0:
 	set = set_menu_opened_amount
-	
+@export_range(0.1,10.0,0.01, "or_greater") var animation_duration := 2.3
+var is_currently_opening := false
+var _tween: Tween
 func set_menu_opened_amount(amount: float) -> void:
 	menu_opened_amount = amount
 	visible = amount > 0
@@ -15,3 +17,14 @@ func set_menu_opened_amount(amount: float) -> void:
 	(_blur_color_rect.material as ShaderMaterial).set_shader_parameter("tint_strength", lerpf(0.0, 0.2, amount))
 	_ui_panel_container.modulate.a = amount
 	
+func toggle() -> void:
+	is_currently_opening = not is_currently_opening
+	var duration := animation_duration
+	if _tween != null:
+		if not is_currently_opening:
+			duration = _tween.get_total_elapsed_time()
+		_tween.kill()
+	_tween = create_tween()
+	_tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	var target_amount := 1.0 if is_currently_opening else 0.0
+	_tween.tween_property(self,"menu_opened_amount", target_amount, duration)
